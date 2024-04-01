e51c99c07707a9ee547a5c21145f00f72950aaf4
Deamkinson <50719345+Deamkinson@users.noreply.github.com>
2024-04-01 13:59:46 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 from dateutil.parser import parse
 import streamlit as st
 import re
+import nltk
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
